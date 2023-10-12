0301559a5dae269c902840799e4b743bff7f890c
java@lan <354801380@qq.com>
2023-10-12 19:18:33 +0800

    ”fix:增加注释“

--- src/router/index.js
+++ src/router/index.js
@@ -15,15 +15,6 @@ VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch((err) => err);
 };
 
-// let autoRoutes = autoRouter({
-//   // 页面级的.vue存放位置，必传
-//   rc: require.context('@/pages', true, /\.vue$/),
-//   // '/'的重定向，可选，默认为''
-//   redirect: '/test',
-//   // 页面级的.vue存放的文件夹，可选，默认为:views
-//   rootFile: 'pages',
-// })
-//
 
 Vue.use(VueRouter);
 //静态路由，不需要做登录拦截的路由
@@ -49,12 +40,11 @@ const staticRoutes = [
 //动态路由，需要做登录拦截的路由，并且可以用做菜单的渲染
 export const dynamicRoutes = gertRouterArray(require.context('./', true, /\/([^/]+)\/index\.js$/));
 
-// 递归生成 dynamicRoutesMap
+// 递归生成 dynamicRoutesMap，方便通过name获取，因为递归得关系很难拿到
 function flattenRoutes(routes, result = {}) {
   routes.forEach((route) => {
     const {children, ...routeInfo} = route;
     result[routeInfo.name] = routeInfo;
-
     if (children && children.length > 0) {
       flattenRoutes(children, result);
     }
@@ -77,13 +67,13 @@ router.beforeEach((to, from, next) => {
     let myToken = localStorage.getItem('myToken');
     if (myToken) {
       next();
+      //一定在next跳转方法以后再添加tags.不然kepp-alivue那里就提前有了该路径，不知道该走缓存还是不走
       if (!store.getters['tags/hasTagsName'](to.name)) {
         store.dispatch("tags/add", {name: to.name, meta: to.meta})
       } else {
         //不能把to全部塞进去,因为有些属性事箭头函数 json转化不了 会报错
         store.dispatch("tags/change", {name: to.name, meta: to.meta})
       }
-
     } else {
       next('/login');
     }
