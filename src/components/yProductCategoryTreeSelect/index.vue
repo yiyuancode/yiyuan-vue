028ff02f0251c2e0b0f500383a3c3051f6798736
java@lan <354801380@qq.com>
2023-10-26 20:02:59 +0800

    feat:新增商品分类组件

---
+++ src/components/yProductCategoryTreeSelect/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div style="width: 100%;">
+    <a-tree-select
+      v-model="value"
+      style="width: 100%;"
+      :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+      :allowClear="allowClear"
+      :multiple="multiple"
+      :tree-data="treeData"
+      :placeholder="placeholder"
+      :replaceFields="replaceFields"
+      :tree-default-expand-all="treeDefaultExpandAll"
+      @change="change"
+    >
+    </a-tree-select>
+  </div>
+</template>
+<script>
+  import {getProductCategoryTreeList} from "@/api/ptm/productCategory.js";
+  export default {
+    props: {
+      allowClear: {
+        type: Boolean,
+        default: function () {
+          return false;
+        }
+      },
+      multiple: {
+        type: Boolean,
+        default: function () {
+          return true;
+        }
+      },
+      placeholder: {
+        type: String,
+        default: function () {
+          return "请选择";
+        }
+      },
+      treeDefaultExpandAll: {
+        type: Boolean,
+        default: function () {
+          return false;
+        }
+      },
+      replaceFields: {
+        type: Object,
+        default: function () {
+          return {children: 'children', title: 'name', key: 'id', value: 'id'};
+        }
+      },
+
+    },
+    data() {
+      return {
+        treeData: [],
+        value: []
+      };
+    },
+    computed: {},
+    created() {
+      this.getData();
+    },
+    methods: {
+      async getData() {
+        let treeList = await getProductCategoryTreeList();
+        this.treeData = treeList;
+      },
+      change(value, label, extra) {
+        this.$emit('input', this.value);
+      }
+    }
+  };
+</script>
+<style lang="less" scoped>
+  .ant-advanced-search-form {
+    padding: 10px;
+
+    /deep/ .ant-form-item {
+      display: flex;
+
+      .ant-form-item-label {
+        width: 100px;
+        white-space: nowrap; /* 防止文本换行 */
+        overflow: hidden; /* 超出部分隐藏 */
+        text-overflow: ellipsis; /* 显示省略符号 */
+      }
+
+      .ant-form-item-control-wrapper {
+        flex: 1;
+
+        .ant-calendar-picker {
+          width: 100% !important;
+        }
+      }
+    }
+  }
+
+  .y-tools {
+    height: 100px;
+    width: 100%;
+    background: red;
+  }
+
+  .y-table {
+    margin: 20px;
+  }
+</style>
