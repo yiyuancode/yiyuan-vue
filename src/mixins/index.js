8335e425f586eb76d8146f9169521cdace8d3890
java@lan <354801380@qq.com>
2023-11-04 10:24:33 +0800

    feat:更新服务端接口地址

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -6,7 +6,7 @@ export default {
           globalConfig: {
             imgBaseUrl: process.env.VUE_APP_IMG_BASE_URL
           }
-          // cjImgBaseUrl: "http://106.54.87.159:50008/mallgoods-dev",
+          // cjImgBaseUrl: "http://116.63.167.31:40009/mallgoods-dev",
           // imgBaseUrl: "https://h5.haomeiwei.top",
           // //ossUploadUrl: "http://localhost:8081/mall-api/admin/minio/upload",
           // ossUploadUrl: "https://h5.haomeiwei.top/mall-api-dev/mall-api/admin/minio/upload",
